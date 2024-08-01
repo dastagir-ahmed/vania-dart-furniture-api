d4828e87f263c1d5a168eb47af7639710b10f17d
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-08-01 10:36:36 +0800

    added line items and order detail

--- lib/app/http/controllers/order_controller.dart
+++ lib/app/http/controllers/order_controller.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
 
 import 'package:decimal/decimal.dart';
-import 'package:stripe/stripe.dart';
 import 'package:vania/vania.dart';
 import 'package:vania_furniture_api/app/models/order.dart';
 import 'package:vania_furniture_api/app/models/order_detail.dart';
 import 'package:vania_furniture_api/app/models/product_bin.dart';
 
 class OrderController extends Controller {
-  placeOrder(Request request) async {
+ Future<Response> placeOrder(Request request) async {
     try {
       final userId = Auth().id();
 
@@ -31,71 +30,53 @@ class OrderController extends Controller {
       final orderNum = DateTime.now().millisecondsSinceEpoch;
       double amountTotal = 0;
 
-      //get order info
-      //get the amount
-      //convert the amount to cents
-      //LineItem refers to the order, number, amount.....
-      List<LineItem> lineItems = [];
+      final String apiKey = "stripe secret key";
+      final Uri url = Uri.https('api.stripe.com', '/v1/checkout/sessions');
 
-      for (var element in dataList) {
-        //"12.303"-->12.303
+      final Map<String, String> body = {
+        'payment_methods_types[]':'card',
+        'mode':'payment',
+        'success_url':env('APP_URL') + '/success.html',
+        'cancel_url':env('APP_URL')+'/cancel.html',
+        'metadata[order_id]':"${orderNum}"
+      };
+
+      for(int i=0; i<dataList.length; i++){
+        final element = dataList.elementAt(i);
         final price = Decimal.parse(element.price!);
         //12.30
-        final priceDouble = price.floor(scale: 2).toDouble();
-
-        //12.30*100=>1230
-        int priceInt = (priceDouble * 100).toInt();
-
-        //12.30*3
-        final amountSum = priceDouble * element.cartNumber!;
-        //add all the items in a loop
+        var priceDouble = price.floor(scale: 2).toDouble();
+        //1230 send to stripe
+        final priceInt = (priceDouble*100).toInt();
+        //keep for ourself in the database
+        var amountSum = priceDouble*element.cartNumber!;
+        // total amount for all the items in the cart
         amountTotal = amountTotal + amountSum;
 
-        lineItems.add(LineItem(
-            priceData: PriceData(
-                currency: "usd",
-                productData: ProductData(name: "${element.title}"),
-                unitAmount: priceInt),
-            quantity: element.cartNumber));
-
-        await OrderDetail().query().insert({
-          "user_id": userId,
-          "amount": amountSum,
-          "order_num": orderNum,
-          "product_id": element.id,
-          "title": element.title,
-          "num": element.cartNumber,
-          "price": element.price,
-          "pic": element.thumbnail,
-          "created_at": DateTime.now(),
-          "updated_at": DateTime.now()
+        body.addAll({
+          'line_items[$i][price_data][currency]':'usd',
+          'line_items[$i][price_data][product_data][name]':'${element.title}',
+          'line_items[$i][price_data][unit_price]':priceInt.toString(),
+          'line_items[$i][quantity]':element.cartNumber.toString(),
         });
 
-         
+        await OrderDetail().query().insert({
+          "user_id":userId,
+          "amount":amountSum,
+          "order_num":orderNum,
+          "product_id":element.id,
+          "title":element.title,
+          "price":element.price,
+          "num":element.cartNumber,
+          "pic":element.thumbnail,
+          "created_at":DateTime.now(),
+          "updated_at":DateTime.now(),
+        });
       }
-
-      await Order().query().insert({
-        "user_id": userId,
-        "amount_total": amountTotal.toStringAsFixed(2),
-        "order_num": orderNum,
-        "created_at": DateTime.now(),
-        "updated_at": DateTime.now()
-      });
-
-      final stripe = Stripe(
-          "sk_test_51NDjUSDcNOyMHK5HXM82Vp9SmGNrUbu4wTpn4KsvytjoVxnJXo6243K262SqdHHypMIloirm1xSVEnqW0edTSH1N00h9q3RWf3");
-      final checkoutData = CreateCheckoutSessionRequest(
-          successUrl: env('APP_URL') + '/success.html',
-          cancelUrl: env('APP_URL') + '/cancel.html',
-          mode: SessionMode.payment,
-          clientReferenceId: "${orderNum}",
-          paymentMethodTypes: [PaymentMethodType.card],
-          lineItems: lineItems);
-      final checkoutSession = await stripe.checkoutSession.create(checkoutData);
       
       return Response.json({
         "code":200,
-        "data":checkoutSession.id,
+        "data":"",
         "msg":"Payment success"
       });
 
@@ -109,14 +90,19 @@ class OrderController extends Controller {
   }
 
   Future<Response> webhook(Request request){
-
+   //print("0");
     final event = request.all();
+    //print("1 ${event}");
+    print(event);
     if(event['type']=='checkout.session.completed'){
+      print("2");
       final session = event['data']['object'];
       final orderId = session['metadata']['order_id'];
       Order().query().where("order_num", orderId).update({"status":1});
+      print("3");
       return Response.json("Payment success");
     }
+    print("4");
     return Response.json("Unknown events");
 
   }
